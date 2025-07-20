2d53fe931d6d3ca1e27974d44c6d9e8fe5d58194
Roshan4546 <roshanpatra275@gmail.com>
2025-07-20 19:04:23 +0530

    changes in class-4

--- class-4/src/App.js
+++ class-4/src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [text, setText] = useState('');
+  // const [name, setName] = useState('love');
+
+  // variation-1 -> redering every time
+
+  // useEffect(() => {
+  //   console.log("ui render is done.");
+  // })
+  
+
+  // variation 2 -> first render
+  // ! due to react have strictmode so that when rendering it print two times.
+  // ? to overcome delete strictmode tag.
+  // useEffect(() => {
+  //   console.log("ui rendering is done");
+  // }, []);
+
+
+  // variation-3 -> on first render + whenever depended.
+
+  // useEffect(() => {
+  //   console.log("change observed");
+  // }, [name]);
+
+  // variation-4 ->to handle unamounting of a component
+
+  useEffect(() => {
+  console.log("Listener add.");
+  // Add event listener here
+
+  return () => {
+    console.log("Listener delete.");
+    // Clean up event listener here
+  };
+}, [text]);
+
+
+  function changeHandler(event) {
+    console.log(text);
+    setText(event.target.value);
+  }
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type='text' onChange={changeHandler}></input>
     </div>
   );
 }
